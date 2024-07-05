a47461745dd38402ac0ea1f2201064574cdfe8a5
AAACCCVV <158253268+AAACCCVV@users.noreply.github.com>
2024-07-05 15:15:33 +0800

    Update and rename test_2.ts to test_3.ts

--- test_2.ts
+++
@@ -1,9 +0,0 @@
-const { ccclass, property } = cc._decorator;
-
-@ccclass
-export default class RemoteScriptLoader extends cc.Component {
-    console.log('Guide!!!');
-    function Guide() {
-    console.log('Guide!!!');
-}
-}
---
+++ test_3.ts
@@ -0,0 +1,9 @@
+// const { ccclass, property } = cc._decorator;
+
+// @ccclass
+// export default class RemoteScriptLoader extends cc.Component {
+console.log('Guide!!!');
+//     function Guide() {
+//     console.log('Guide!!!');
+// }
+// }
